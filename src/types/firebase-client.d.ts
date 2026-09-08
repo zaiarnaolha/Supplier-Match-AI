35585f109641c59b66c22d964bb93f8dc3521c98
zaiarnaolha <zaiarnaolha@gmail.com>
2026-09-08 21:07:30 +0300

    Type Firestore browser helpers

--- src/types/firebase-client.d.ts
+++ src/types/firebase-client.d.ts
@@ -6,10 +6,19 @@ type SupplierMatchFirebaseUser = {
   displayName: string | null;
 };
 
+type SupplierMatchSearchInput = {
+  query: string;
+  criteria: Record<string, unknown>;
+};
+
 type SupplierMatchFirebase = {
   signInWithGoogle: () => Promise<{ user: SupplierMatchFirebaseUser }>;
   subscribeToAuth: (callback: (user: SupplierMatchFirebaseUser | null) => void) => () => void;
   signOutUser: () => Promise<void>;
+  ensureCurrentUser: () => Promise<void>;
+  startSearch: (input: SupplierMatchSearchInput) => Promise<string | null>;
+  completeSearch: (searchId: string | null, results: unknown[]) => Promise<void>;
+  listSearches: () => Promise<Array<Record<string, unknown>>>;
 };
 
 declare global {
